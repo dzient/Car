f9071a31c10655b6cab8f91c630a0fe382a07af3
dzient <34776686+dzient@users.noreply.github.com>
2022-07-14 10:22:38 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+//-----------------------------------------------------------------
+//Car
+//
+// Name: David Zientara
+// Date: 7-13-2022
+// Comments: An exercise in classes
+// Added methods per the instructions
+//-----------------------------------------------------------------
+
+
+
+class Car
+{
+    //Added Make, Model and Year
+    //Made Make + Model nullable
+    public string? Make { get; set; }
+    public string? Model { get; set; }
+    public int Year { get; set; }
+
+
+    public static void Main()
+    {
+       
+        //Create a car and change the properties
+        Car car = new Car();
+        car.Make = "Volkswagen";
+        car.Model = "Golf";
+        car.Year = 1992;
+        //Print it out
+        Console.WriteLine($"Your car is a {car.Year} {car.Make} {car.Model} ");
+
+    }
+
+}
